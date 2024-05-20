e5d556ce405045794ece4b12d2c5c368d04c05b3
jessiejk7777 <123924954+jessiejk7777@users.noreply.github.com>
2024-05-20 11:43:38 +0530

    chatbot code.py

--- chatbot code.py
+++ chatbot code.py
@@ -1,71 +1,118 @@
-#!/usr/bin/env python
-# coding: utf-8
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+import csv
+from datetime import date
+import math as m
 
-# In[ ]:
+today = date.today()
+
+# dd/mm/YY
+d1 = today.strftime("%d/%m/%Y")
+days= int(d1[:2])
+months= int(d1[3:5])
+years= int(d1[6:10])
+weeknumber= 4*(months-1)+ m.ceil((days+2)/7) #+2 so weekcount starts on sundays 
 
 
-import datetime
 
-class TaskScheduler:
-    def __init__(self):
-        self.tasks = {}
+with open("taskbotpis_read.csv", newline='') as csvfile:
+    reader= csv.reader(csvfile)
+    #reader is a list of lists each one containing the tasks for the 
+    #user with the following syntax ['id', task1, ..., taskn, 'extrainfo']
+    #extrainfo for example being the shoppinglist or the menus in our case
+    tasklist= []
+    for row in reader:
+        tasklist.append(row)
 
-    def add_task(self, task, time):
-        self.tasks[task] = time
-        print(f"Task '{task}' scheduled for {time}")
+TOKEN = (
+    open("token.txt").read().strip()
+)  # constant which uses telegram to recognize the bot
 
-    def list_tasks(self):
-        if self.tasks:
-            print("Scheduled Tasks:")
-            for task, time in self.tasks.items():
-                print(f"- {task}: {time}")
+def startingmessage(update, context):
+    """Sends a message introducing the bot."""
+
+    text_send = "Bones, soc el taskbot del pis de les corts. Selecciona usuari: /crossfit, /puigde, /pitus"
+    context.bot.send_message(
+        chat_id=update.effective_chat.id,
+        text=text_send,
+    )
+
+def tasks_crossfit(update, context):
+    tasks(update, context, 0)
+def tasks_pitus(update, context):
+    tasks(update, context, 1)
+def tasks_puigde(update, context):
+    tasks(update, context, 2)
+
+#id 0 for crossfit, 1 for pitus 2 for puigde
+def tasks(update, context, id):
+    """Sends a message of the tasks each person has assigned for the week."""
+    text_send=''
+    if (id==0): 
+        text_send += "Bones Crossfit, les teves tasques d'aquesta setmana son:"
+        for i in range(len(tasklist[0])-1):
+            text_send+= '->'+ tasklist[0][i] + '\n'
+        text_send+= '\n'
+        text_send += 'Recorda que la llista de la compra actual és:\n'
+        text_send += tasklist[0][-1] +'\n'*2
+
+        if (weeknumber%3==2):
+            text_send+= 'Recorda que aquesta setmana les aigues et toquen a tu ;)'
+
+    if (id==1):
+        text_send+= "Bones Pitus, les teves tasques d'aquesta setmana son: \n ->"
+        if (weeknumber%2==1):
+            text_send+= tasklist[1][3] + '\n'
         else:
-            print("No tasks scheduled for today.")
+            text_send+= tasklist[1][2] + '\n'
+        text_send+= '->'+ tasklist[1][1] + '\n'
+        for i in range(4,len(tasklist[1])):
+            text_send+= '->' +tasklist[1][i] + '\n'
+        text_send+= '\n'
+        
+        if (weeknumber%3==0):
+            text_send+= 'Recorda que aquesta setmana les aigues et toquen a tu ;)'
 
-    def remove_task(self, task):
-        if task in self.tasks:
-            del self.tasks[task]
-            print(f"Task '{task}' removed from schedule.")
+    if (id==2):
+        text_send+= "Bones Puigde, les teves tasques d'aquesta setmana son: \n ->"
+        if (weeknumber%2==0):
+            text_send+= tasklist[2][3] + '\n'
         else:
-            print(f"No task named '{task}' found in schedule.")
+            text_send+= tasklist[2][2] + '\n'    
+        text_send+= '->'+ tasklist[2][1] + '\n'
+        for i in range(4,len(tasklist[2])-1):
+            text_send+= '->' +tasklist[2][i] + '\n'
+        text_send+= '\n'
+        text_send+= "Recorda que els menús actuals son: " + tasklist[2][len(tasklist[2])-1] + '. \n\n'
+        if (weeknumber%3==1):
+            text_send+= "Recorda que aquesta setmana les aigues et toquen a tu ;)"
+        
 
-    def clear_tasks(self):
-        self.tasks = {}
-        print("All tasks cleared.")
+    context.bot.send_message(
+        chat_id=update.effective_chat.id,
+        text=text_send,
+    )
 
-    def assist(self):
-        print("How can I assist you today?")
-        while True:
-            command = input("Enter 'schedule' to add a task, 'list' to see scheduled tasks, 'remove' to remove a task, 'clear' to clear all tasks, or 'exit' to quit: ").lower()
-            
-            if command == 'schedule':
-                task = input("Enter the task: ")
-                time = input("Enter the time for the task (e.g., '14:00'): ")
-                self.add_task(task, time)
-            elif command == 'list':
-                self.list_tasks()
-            elif command == 'remove':
-                task = input("Enter the task to remove: ")
-                self.remove_task(task)
-            elif command == 'clear':
-                confirm = input("Are you sure you want to clear all tasks? (yes/no): ").lower()
-                if confirm == 'yes':
-                    self.clear_tasks()
-                else:
-                    print("Clear operation canceled.")
-            elif command == 'exit':
-                print("Goodbye!")
-                break
-            else:
-                print("Invalid command. Please try again.")
+# creates objects to work with Telegram
+updater = Updater(token=TOKEN, use_context=True)
+dispatcher = updater.dispatcher
 
-if __name__ == "__main__":
-    scheduler = TaskScheduler()
-    scheduler.assist()
+# When bot recieves the command (eg: start), function /start is executed
+dispatcher.add_handler(CommandHandler("i", startingmessage))
+dispatcher.add_handler(CommandHandler("ii", startingmessage))
+dispatcher.add_handler(CommandHandler("iii", startingmessage))
+dispatcher.add_handler(CommandHandler("iiii", startingmessage))
+dispatcher.add_handler(CommandHandler("iiiii", startingmessage))
+dispatcher.add_handler(CommandHandler("iiiiii", startingmessage))
+dispatcher.add_handler(CommandHandler("iiiiiii", startingmessage))
+dispatcher.add_handler(CommandHandler("iiiiiiiii", startingmessage))
+dispatcher.add_handler(CommandHandler("crossfit", tasks_crossfit))
+dispatcher.add_handler(CommandHandler("pitus", tasks_pitus))
+dispatcher.add_handler(CommandHandler("puigde", tasks_puigde))
 
 
-# In[ ]:
+# bot is started
+updater.start_polling()
 
-
-
+# command necessary if MacOS is used
+updater.idle()
 
